a7737e518eb39f0823a1823864f371e857c828a8
IHIRINA <2582067027@qq.com>
2025-04-16 20:21:23 +0800

    false

--- demo/src/main/java/com/example/demo/mapper/UserMapper.java
+++ demo/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,13 +1,12 @@
 package com.example.demo.mapper;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Insert;
-import com.example.demo.entity.User; // Ensure this is the correct package for the User class
+import com.example.demo.entity.User;
 
 @Mapper
 public interface UserMapper {
-
-    @Insert("insrt into `user` (username , paswword , name , phone , email , address , avatar)" +
-            "values (#{username}, #{password}, #{name}, #{phone}, #{email}, #{address}, #{avatar})")
+    @Insert("INSERT INTO `user` (username, password, name, phone, email, address, avatar) " +
+            "VALUES (#{username}, #{password}, #{name}, #{phone}, #{email}, #{address}, #{avatar})")
     void insertUser(User user);
 }
-
